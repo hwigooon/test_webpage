3257d0f9a7d418554a4093732a1b55fd1c9681b0
bumhwi.kim <bumhwi.kim@etri.re.kr>
2020-07-01 11:34:20 +0900

    Updates

---
+++ precache-manifest.26dd0e92122a937f939567a235085357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b59862f563da2e123272b2b21d9e28f",
+    "url": "/test_webpage/index.html"
+  },
+  {
+    "revision": "41bb95b9a64c8193155e",
+    "url": "/test_webpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d27d57a228ccebd468c",
+    "url": "/test_webpage/static/js/2.11be9d4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_webpage/static/js/2.11be9d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41bb95b9a64c8193155e",
+    "url": "/test_webpage/static/js/main.f97ce9a1.chunk.js"
+  },
+  {
+    "revision": "69258808b879f3602165",
+    "url": "/test_webpage/static/js/runtime-main.f6838347.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_webpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
